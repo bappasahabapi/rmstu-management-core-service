4dee0ec583eadb8e9b6429e0e263c6df1db971a9
Bappa Saha <bappasaha161@gmail.com>
2023-09-02 23:52:11 +0600

    [09] pagination added in service

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,5 +1,6 @@
 import { AcademicSemester, PrismaClient } from "@prisma/client";
 import { IGenericResponse } from "../../../interfaces/common";
+import { paginationHelpers } from "../../../helpers/paginationHelper";
 
 
 
@@ -12,14 +13,22 @@ const insertInDB=async (academicSemesterData:AcademicSemester):Promise<AcademicS
     return result;
 };
 
-const getAllFromDB =async():Promise<IGenericResponse<AcademicSemester[]>> =>{
-    const result =await prisma.academicSemester.findMany();
+const getAllFromDB =async(filters,options):Promise<IGenericResponse<AcademicSemester[]>> =>{
+
+    //todo: for pagination
+    const {page,limit,skip}=paginationHelpers.calculatePagination(options);
+
+
+    const result =await prisma.academicSemester.findMany({
+        skip,
+        take:limit
+    });
     const total =await prisma.academicSemester.count();
     return{
         meta:{
             total,
-            page:1,
-            limit:10
+            page,
+            limit
         },
         data:result
     }
