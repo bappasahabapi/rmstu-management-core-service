e38c6f3829a98b2d8d03542a5b04b00f83ffcd6e
Bappa Saha <bappasaha161@gmail.com>
2023-09-02 23:40:15 +0600

    [07] console log the filters query and options

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,33 +1,46 @@
-import {Request, Response } from "express";
-import { AcademicSemesterService } from "./academicSemester.service";
-import sendResponse from "../../../shared/sendResponse";
 import { AcademicSemester } from "@prisma/client";
+import { Request, Response } from "express";
 import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
+import sendResponse from "../../../shared/sendResponse";
+import { AcademicSemesterService } from "./academicSemester.service";
+
 
 const insertInDB = catchAsync(async (req: Request, res: Response) => {
-        const result = await AcademicSemesterService.insertInDB(req.body);
-        sendResponse<AcademicSemester>(res,{
-            statusCode:httpStatus.OK,
-            success:true,
-            message:"Academic Semester created successfully",
-            data:result
-        })
- 
+    const result = await AcademicSemesterService.insertInDB(req.body);
+    sendResponse<AcademicSemester>(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Academic Semester created successfully",
+        data: result
+    })
+
 });
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-        const result = await AcademicSemesterService.getAllFromDB();
-        sendResponse(res,{
-            statusCode:httpStatus.OK,
-            success:true,
-            message:" Retrived Academic Semester data successfully",
-            meta:result.meta,
-            data:result.data
-        })
+
+    //todo: keep the filterable data in one variable
+    console.log(req.query);
+    const filters=pick(req.query,['searchTerm','code','year']);
+    const options =pick(req.query,['limit', 'page','sortBy','sortOrder']);
+    
+    console.log("filters",filters);
+    console.log("options",options);
+
+
+
+    const result = await AcademicSemesterService.getAllFromDB();
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: " Retrived Academic Semester data successfully",
+        meta: result.meta,
+        data: result.data
+    })
 });
 
-export const  AcademicSemesterController ={
+export const AcademicSemesterController = {
     insertInDB,
     getAllFromDB
 }
