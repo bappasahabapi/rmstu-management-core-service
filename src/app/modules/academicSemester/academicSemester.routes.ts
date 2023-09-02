79a465e0fdcc340655e9c465619ad248205d2b86
Bappa Saha <bappasaha161@gmail.com>
2023-09-02 23:01:10 +0600

    [04] zod validation middleware is added in route

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemeter.validation';
 
 
-const router=express.Router();
+const router = express.Router();
 
 
-router.post('/',AcademicSemesterController.insertInDB);
+router.post(
+    '/',
+    validateRequest(AcademicSemesterValidation.create), 
+    AcademicSemesterController.insertInDB);
 
 
-export const AcademicSemesterRoutes =router;
+export const AcademicSemesterRoutes = router;
