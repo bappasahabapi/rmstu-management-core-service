394075e1aa9903f5c138e5425558e35fd5a6054d
Bappa Saha <bappasaha161@gmail.com>
2023-09-04 00:31:08 +0600

    [c1-05-01-department route is added]

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 
 const router = express.Router();
 
@@ -13,7 +14,11 @@ const moduleRoutes = [
   {
     path: "/academic-faculties",
     route: AcademicFacultyRoutes
-  }
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
