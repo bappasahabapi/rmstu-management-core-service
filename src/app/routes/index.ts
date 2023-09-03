4e313838ae8618eb657e8273247fcefd504f4f59
Bappa Saha <bappasaha161@gmail.com>
2023-09-03 22:36:26 +0600

    [c1.04-02] route is export in main index file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 
 const router = express.Router();
 
@@ -8,6 +9,10 @@ const moduleRoutes = [
   {
     path: "/academic-semesters",
     route: AcademicSemesterRoutes
+  },
+  {
+    path: "/academic-faculties",
+    route: AcademicFacultyRoutes
   }
 ];
 
