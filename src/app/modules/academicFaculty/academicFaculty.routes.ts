35eea1180906dafbadcf8a9ba4201d56f46c9f9b
Bappa Saha <bappasaha161@gmail.com>
2023-09-03 22:51:18 +0600

    [c1.04-04] validation is added in create route

--- src/app/modules/academicFaculty/academicFaculty.routes.ts
+++ src/app/modules/academicFaculty/academicFaculty.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { AcademicFacultyController } from './academicFaculty.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 
 const router= express.Router();
 
 router.post(
     '/',
+    validateRequest(AcademicFacultyValidation.create),
     AcademicFacultyController.insertInDB
     );
 
