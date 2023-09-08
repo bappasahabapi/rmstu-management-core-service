639ce739840d353d16aba803d087f289ffbc7931
Bappa Saha <bappasaha161@gmail.com>
2023-09-09 01:05:10 +0600

    "c2.11-004" room api is done

--- src/app/modules/room/room.controller.ts
+++ src/app/modules/room/room.controller.ts
@@ -3,6 +3,8 @@ import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { RoomService } from "./room.service";
+import pick from "../../../shared/pick";
+import { roomFilterableFields } from "./room.constants";
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     const result = await RoomService.insertIntoDB(req.body);
@@ -12,9 +14,58 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
         message: "Room created successfully!",
         data: result
     })
-})
+});
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, roomFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await RoomService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Rooms fetched successfully',
+        meta: result.meta,
+        data: result.data
+    });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await RoomService.getByIdFromDB(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Room fetched successfully',
+        data: result
+    });
+});
+
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await RoomService.updateOneInDB(id, req.body);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Room updated successfully',
+        data: result
+    });
+});
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await RoomService.deleteByIdFromDB(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Room deleted successfully',
+        data: result
+    });
+});
 
 export const RoomController = {
-    insertIntoDB
+    insertIntoDB,
+    getAllFromDB,
+    getByIdFromDB,
+    updateOneInDB,
+    deleteByIdFromDB
 }
--- src/app/modules/room/room.routes.ts
+++ src/app/modules/room/room.routes.ts
@@ -2,14 +2,30 @@ import express from 'express';
 // import { ENUM_USER_ROLE } from '../../../enums/user';
 // import auth from '../../middlewares/auth';
 import { RoomController } from './room.controller';
+import { RoomValidation } from './room.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
 
 router.post(
     '/',
+    validateRequest(RoomValidation.create),
     // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
     RoomController.insertIntoDB
-)
+);
+
+router.patch(
+    '/:id',
+    validateRequest(RoomValidation.update),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    RoomController.updateOneInDB
+);
+
+router.delete(
+    '/:id',
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    RoomController.deleteByIdFromDB
+);
 
 
 export const roomRoutes = router;
