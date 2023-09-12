690dabdb03767e61d9ead226a0c58896ab412d6f
Bappa Saha <bappasaha161@gmail.com>
2023-09-12 22:29:02 +0600

    06-add status validation

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,7 +1,31 @@
-import { SemesterRegistration } from "@prisma/client";
+import { SemesterRegistration, SemesterRegistrationStatus } from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const insertIntoDB =async(data:SemesterRegistration):Promise<SemesterRegistration > =>{
+
+    const isAnySemesterRegUpcomingOrOngoing =await prisma.semesterRegistration.findFirst({
+        where:{
+            OR:[
+                {
+                    status:SemesterRegistrationStatus.UPCOMING
+                },
+                {
+                    status:SemesterRegistrationStatus.ONGING
+                }
+            ]
+        }
+    });
+
+    if (isAnySemesterRegUpcomingOrOngoing) {
+        throw new ApiError(httpStatus.BAD_REQUEST,
+            `Thers is already an ${isAnySemesterRegUpcomingOrOngoing.status} registration.`
+        )
+    }
+
+
+
     const result = await prisma.semesterRegistration.create({
         data
     });
