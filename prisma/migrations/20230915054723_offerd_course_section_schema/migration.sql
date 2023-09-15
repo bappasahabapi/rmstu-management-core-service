4e8cbe11408501884c8d2433f85969925cb68e4e
Bappa Saha <bappasaha161@gmail.com>
2023-09-15 11:51:33 +0600

    🇧🇩--01-Schema Created

--- prisma/migrations/20230915054723_offerd_course_section_schema/migration.sql
+++ prisma/migrations/20230915054723_offerd_course_section_schema/migration.sql
@@ -1,6 +1,9 @@
 -- CreateEnum
 CREATE TYPE "SemesterRegistrationStatus" AS ENUM ('UPCOMING', 'ONGOING', 'ENDED');
 
+-- CreateEnum
+CREATE TYPE "WeekDays" AS ENUM ('SATURDAY', 'SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY');
+
 -- CreateTable
 CREATE TABLE "academic_semesters" (
     "id" TEXT NOT NULL,
@@ -169,6 +172,22 @@ CREATE TABLE "offered_course_sections" (
     CONSTRAINT "offered_course_sections_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "offered_course_class_schedules" (
+    "id" TEXT NOT NULL,
+    "startTime" TEXT NOT NULL,
+    "endTime" TEXT NOT NULL,
+    "dayOfWeek" "WeekDays" NOT NULL DEFAULT 'SATURDAY',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "offeredCourseSectionId" TEXT NOT NULL,
+    "semesterRegistrationId" TEXT NOT NULL,
+    "roomId" TEXT NOT NULL,
+    "facultyId" TEXT NOT NULL,
+
+    CONSTRAINT "offered_course_class_schedules_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "academic_departments" ADD CONSTRAINT "academic_departments_academicFacultyId_fkey" FOREIGN KEY ("academicFacultyId") REFERENCES "academic_faculties"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -219,3 +238,15 @@ ALTER TABLE "offered_course_sections" ADD CONSTRAINT "offered_course_sections_of
 
 -- AddForeignKey
 ALTER TABLE "offered_course_sections" ADD CONSTRAINT "offered_course_sections_semesterRegistrationId_fkey" FOREIGN KEY ("semesterRegistrationId") REFERENCES "semester_registrations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "offered_course_class_schedules" ADD CONSTRAINT "offered_course_class_schedules_offeredCourseSectionId_fkey" FOREIGN KEY ("offeredCourseSectionId") REFERENCES "offered_course_sections"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "offered_course_class_schedules" ADD CONSTRAINT "offered_course_class_schedules_semesterRegistrationId_fkey" FOREIGN KEY ("semesterRegistrationId") REFERENCES "semester_registrations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "offered_course_class_schedules" ADD CONSTRAINT "offered_course_class_schedules_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "rooms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "offered_course_class_schedules" ADD CONSTRAINT "offered_course_class_schedules_facultyId_fkey" FOREIGN KEY ("facultyId") REFERENCES "faculties"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
